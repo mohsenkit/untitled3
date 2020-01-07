6f632e64bb048c6cfaceeb120795278bfcdc8c36
mohsenkit <mohsen.mahbubi@yahoo.com>
2020-01-08 00:42:52 +0330

    1

---
+++ index.php
@@ -0,0 +1,215 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="styles/stylesheet.css">
+    <link rel="stylesheet" href="styles/css/all.css">
+
+    <title>محسن کیت</title>
+</head>
+<body>
+
+<nav id="nav">
+    <div class="nav container">
+
+        <div class="right">
+            <ul>
+                <li><a href="#">خانه</a></li>
+                <li><a href="#">محصولات</a></li>
+                <li><a href="#">حساب کاربری</a></li>
+                <li><a href="#">سبد خرید</a></li>
+                <li><a href="#">تسویه حساب</a></li>
+            </ul>
+        </div>
+
+        <div class="left">
+            <ul>
+                <li><a href="#">ورود</a></li>-
+                <li><a href="#">ثبت نام</a></li>
+            </ul>
+
+        </div>
+
+        <div class="clear"></div>
+
+    </div>
+</nav>
+
+<header id="header" class="container">
+    <div class="logo"><img src="images/logo.png" alt="زنبیل"></div>
+    <div class="searchBox">
+        <input type="text" placeholder="کالای موردنظرتان را جستجو کنید ...">
+        <button><i class="fas fa-search"></i></button>
+    </div>
+    <div class="clear"></div>
+</header>
+
+<section id="mainSlider" class="slider">
+    <img src="images/laptop-banner.jpg" alt="banner">
+</section>
+
+<section id="about">
+    <div class="container">
+        <ul>
+
+            <li>
+                <i class="fas fa-shipping-fast"></i>
+                <h3>ارسال رایگان</h3>
+                <p class="desc">متن تستی</p>
+            </li>
+            <li>
+                <i class="far fa-clock"></i>
+                <h3>تحویل سریع</h3>
+                <p class="desc">متن تستی</p>
+            </li>
+            <li>
+                <i class="far fa-thumbs-up"></i>
+                <h3>رضایت مشتریان</h3>
+                <p class="desc">متن تستی</p>
+            </li>
+
+        </ul>
+    </div>
+</section>
+
+<div class="container products-section">
+    <div class="header">
+        <ul>
+            <li><a href="#">جدیدترین</a></li>
+            <li><a href="#">پربازدیدترین</a></li>
+            <li><a href="#">پرفروش ترین</a></li>
+            <li><a href="#">ارزان ترین</a></li>
+            <li><a href="#">گران ترین</a></li>
+        </ul>
+    </div>
+    <div class="products">
+        <ul>
+
+            <li>
+                <img src="images/laptop1.png" alt="LapTop">
+                <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+                <div class="price">
+                    <span class="new-price">200.000 تومان</span>
+                    <span class="old-price">240.000 تومان</span>
+                </div>
+                <a href="#" class="addToCart">افزودن به سبد خرید <i class="fas fa-cart-plus"></i></a>
+            </li>
+
+            <li>
+                <img src="images/laptop1.png" alt="LapTop">
+                <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+                <div class="price">
+                    <span class="new-price">200.000 تومان</span>
+                    <span class="old-price">240.000 تومان</span>
+                </div>
+                <a href="#" class="addToCart">افزودن به سبد خرید<i class="fas fa-cart-plus"></i></a>
+            </li>
+
+            <li>
+                <img src="images/laptop1.png" alt="LapTop">
+                <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+                <div class="price">
+                    <span class="new-price">200.000 تومان</span>
+                    <span class="old-price">240.000 تومان</span>
+                </div>
+                <a href="#" class="addToCart">افزودن به سبد خرید<i class="fas fa-cart-plus"></i></a>
+            </li>
+
+            <li>
+                <img src="images/laptop1.png" alt="LapTop">
+                <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+                <div class="price">
+                    <span class="new-price">200.000 تومان</span>
+                    <span class="old-price">240.000 تومان</span>
+                </div>
+                <a href="#" class="addToCart">افزودن به سبد خرید<i class="fas fa-cart-plus"></i></a>
+            </li>
+
+            <li>
+                <img src="images/laptop1.png" alt="LapTop">
+                <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+                <div class="price">
+                    <span class="new-price">200.000 تومان</span>
+                    <span class="old-price">240.000 تومان</span>
+                </div>
+                <a href="#" class="addToCart">افزودن به سبد خرید<i class="fas fa-cart-plus"></i></a>
+            </li>
+
+            <li>
+                <img src="images/laptop1.png" alt="LapTop">
+                <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+                <div class="price">
+                    <span class="new-price">200.000 تومان</span>
+                    <span class="old-price">240.000 تومان</span>
+                </div>
+                <a href="#" class="addToCart">افزودن به سبد خرید<i class="fas fa-cart-plus"></i></a>
+            </li>
+
+        </ul>
+    </div>
+</div>
+
+<section id="telegramSection">
+    <div class="telegram  container">
+        <div class="telegram-logo"><i class="fab fa-telegram"></i></div>
+        <div class="right">
+            <h2>همه چیز در تلگرام</h2>
+            <p>لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.</p>
+            <a href="#" class="goToTelegram">ورود به کانال تلگرام</a>
+        </div>
+    </div>
+</section>
+
+<div class="container products">
+    <ul>
+
+        <li>
+            <img src="images/laptop1.png" alt="LapTop">
+            <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+            <div class="price">
+                <span class="new-price">200.000 تومان</span>
+                <span class="old-price">240.000 تومان</span>
+            </div>
+            <a href="#" class="addToCart">افزودن به سبد خرید<i class="fas fa-cart-plus"></i></a>
+        </li>
+
+        <li>
+            <img src="images/laptop1.png" alt="LapTop">
+            <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+            <div class="price">
+                <span class="new-price">200.000 تومان</span>
+                <span class="old-price">240.000 تومان</span>
+            </div>
+            <a href="#" class="addToCart">افزودن به سبد خرید<i class="fas fa-cart-plus"></i></a>
+        </li>
+
+        <li>
+            <img src="images/laptop1.png" alt="LapTop">
+            <h2 class="product-title">بهترین لپ تاپ 256 گیگ سال 2019</h2>
+            <div class="price">
+                <span class="new-price">200.000 تومان</span>
+                <span class="old-price">240.000 تومان</span>
+            </div>
+            <a href="#" class="addToCart">افزودن به سبد خرید<i class="fas fa-cart-plus"></i></a>
+        </li>
+
+    </ul>
+</div>
+
+<section id="newsletter">
+    <div class="container">
+        <h2>ثبت نام در خبرنامه</h2>
+        <h3>برای عضویت فرم زیر را تکمیل کنید</h3>
+        <form action="">
+            <input type="text" placeholder="ایمیل خود را وارد کنید و enter بزنید">
+        </form>
+    </div>
+</section>
+
+<footer>
+    <p>تهران سمت راست</p>
+    <p>طراحی شده با <i class="fas fa-coffee"></i> و <i class="fas fa-heartbeat"></i> در xanbil.com</p>
+</footer>
+
+</body>
+</html>
